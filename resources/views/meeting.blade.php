2383bc1e225f704b343c9caba7d16804a5a3a0bf
andyxob <a.xlopot@g,ail.com>
2022-06-14 12:00:14 +0300

    edit times table

--- resources/views/meeting.blade.php
+++ resources/views/meeting.blade.php
@@ -3,32 +3,36 @@
 @section('title', 'Meeting doctor form')
 
 @section('content')
-    <p>Select doctor</p>
-    <select class="form-control mt-2">
+    <form method="post">
+        <p>Select doctor</p>
+        <select class="form-control">
 
 
-        @foreach($doctors as $doctor)
-            <option value="{{$doctor->id}}"
-                    selected>{{$doctor->name}}</option>
-        @endforeach
-    </select>
+            @foreach($doctors as $doctor)
+                <option value="{{$doctor->id}}">{{$doctor->name}}</option>
+            @endforeach
+        </select>
 
-    <p>select massage type </p>
-    <select      class="form-control mt-2">
-                @foreach($massages as $massage)
-            <option value="{{$massage->id}}"
-                    selected>{{$massage->name}}</option>
-        @endforeach
-    </select>
+        <p>select massage type </p>
+        <select class="form-control mt-2">
+            @foreach($massages as $massage)
+                <option value="{{$massage->id}}">{{$massage->name}}</option>
+            @endforeach
+        </select>
 
 
-    <p>Select time</p>
-    <select  class="form-control mt-2">
-        @foreach($times as $time)
-            <option value="{{$time->id}}"
-                    selected>{{$time->time}}</option>
-        @endforeach
-    </select>
+        <p>Select time</p>
+        <select class="form-control mt-2">
+            @foreach($times as $time)
+                <option value="{{$time->id}}">{{$time->time}}</option>
+            @endforeach
+        </select>
+
+
+
+    <x-button class="mt-2">
+        confirm
+    </x-button>
 
     </form>
 @endsection
