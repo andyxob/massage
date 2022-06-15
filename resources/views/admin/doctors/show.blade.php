ff4bed8a132bc31dd1daba265f541f9ab9348c9c
andyxob <a.xlopot@g,ail.com>
2022-06-15 13:38:26 +0300

    make crud for massages and making validation for massages and doctors

--- resources/views/admin/doctors/show.blade.php
+++ resources/views/admin/doctors/show.blade.php
@@ -22,13 +22,13 @@
             <td>Surname</td>
             <td>{{$doctor->surname}}</td>
         </tr>
-        <tr
+        <tr>
             <td>Exp</td>
             <td>{{$doctor->exp}}</td>
         </tr>
         <tr>
             <td>Image</td>
-            <td><img src="{{\Illuminate\Support\Facades\Storage::url($doctor->image)}}" width="250" height="250"> </td>
+            <td><img src="{{\Illuminate\Support\Facades\Storage::url($doctor->image)}}" width="250" height="250" alt="This doctor has no image"> </td>
         </tr>
         </tbody>
     </table>
